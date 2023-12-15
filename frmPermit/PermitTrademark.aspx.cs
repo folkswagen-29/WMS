85b3c696a15c0c635ddae0a39021f1afa4699606
folkswagen-29 <folkswagen_29@hotmail.com>
2023-12-15 14:51:36 +0700

    Update Insert PermitTrademark

--- frmPermit/PermitTrademark.aspx.cs
+++ frmPermit/PermitTrademark.aspx.cs
@@ -3,6 +3,7 @@ using onlineLegalWF.userControls;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -42,12 +43,57 @@ namespace onlineLegalWF.frmPermit
 
         protected void btn_save_Click(object sender, EventArgs e)
         {
+            int res = SaveRequest();
 
+            if (res > 0)
+            {
+                Response.Write("<script>alert('Successfully added');</script>");
+                //Response.Redirect("/frmInsurance/InsuranceRequestList");
+            }
+            else
+            {
+                Response.Write("<script>alert('Error !!!');</script>");
+            }
         }
 
         protected void btn_gendocumnt_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private int SaveRequest()
+        {
+            int ret = 0;
+
+            if (doc_no.Text.Trim() == "")
+            {
+                doc_no.Text = zwf.genDocNo("PMT-" + System.DateTime.Now.ToString("yyyy", new CultureInfo("en-US")) + "-", 4);
+            }
+            var xpermit_no = req_no.Text.Trim();
+            var xprocess_id = hid_PID.Value.ToString();
+            var xtof_requester_code = type_requester.SelectedValue;
+            var xpermit_date = System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            var xproject_code = type_project.SelectedValue;
+            var xtof_permitreq_code = "08";
+            var xtrademarks_desc = trademark_desc.Text.Trim();
+            var xstatus = "verify";
+
+            string sql = @"INSERT INTO [dbo].[li_permit_request]
+                                   ([process_id],[permit_no],[permit_date],[tof_requester_code],[project_code],[tof_permitreq_code],[trademarks_desc],[status])
+                             VALUES
+                                   ('" + xprocess_id + @"'
+                                   ,'" + xpermit_no + @"'
+                                   ,'" + xpermit_date + @"'
+                                   ,'" + xtof_requester_code + @"'
+                                   ,'" + xproject_code + @"'
+                                   ,'" + xtof_permitreq_code + @"'
+                                   ,'" + xtrademarks_desc + @"'
+                                   ,'" + xstatus + @"')";
+
+            ret = zdb.ExecNonQueryReturnID(sql, zconnstr);
+
+
+            return ret;
         }
     }
 }
