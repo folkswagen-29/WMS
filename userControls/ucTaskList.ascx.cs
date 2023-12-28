ef6f89b33d8fbcb92fd21c2ede3fe4520c1b6820
Eknawat Choojaisiriroj <eknawat@gmail.com>
2023-12-28 10:25:08 +0700

    update

--- userControls/ucTaskList.ascx.cs
+++ userControls/ucTaskList.ascx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +13,68 @@ namespace onlineLegalWF.userControls
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        public void bindData(string xlogin_name, string xmode)
+        {
+            hidLogin.Value = xlogin_name;
+            hidMode.Value = xmode; 
+
+        }
+        public DataTable getDataStructure() 
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("No",typeof(string));
+            dt.Columns.Add("Subject", typeof(string));
+            dt.Columns.Add("RequestBy", typeof(string));
+            dt.Columns.Add("SubmittedDate", typeof(string));
+            dt.Columns.Add("Status", typeof(string));
+            dt.Columns.Add("LastUpdated", typeof(string));
+            dt.Columns.Add("LastUpdatedBy", typeof(string));
+            return dt; 
+        }
+        public DataTable ini_data()
+        {
+            var dt = getDataStructure(); 
+            for (int i = 1; i<=1; i++)
+            {
+                var dr = dt.NewRow();
+                dr["No"] = i.ToString();
+                dt.Rows.Add(dr); 
+            }
+            return dt; 
+        }
+        public void bind_gv()
+        {
+            switch  (hidMode.Value)
+            {
+                case "MyRequest": {
+
+                     };break;
+                case "MyWorkList":
+                    {
+
+                    }; break;
+                case "CompleteList":
+                    {
+
+                    }; break;
+               
+            }
+        }
+        public DataTable getMyRequest()
+        {
+            var dt = ini_data();
+            return dt; 
+        }
+        public DataTable getMyWorkList()
+        {
+            var dt = ini_data();
+            return dt;
+        }
+        public DataTable getCompleteList()
+        {
+            var dt = ini_data();
+            return dt;
         }
     }
 }
